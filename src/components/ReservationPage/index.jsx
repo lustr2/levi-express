8b99d835ec621426133a60f54cf6c4e14c5eca66
Lucie Struplova <lstruplova@gmail.com>
2024-04-07 14:19:19 +0200

    Vytvoreni routovani

---
+++ src/components/ReservationPage/index.jsx
@@ -0,0 +1,8 @@
+
+export const ReservationPage = () => {
+
+    return (
+        <><h2>Rezervacni stranka ;-)</h2>
+        </>
+    );
+}
--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,25 @@ import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom';
 import { App } from './components/App';
 import './global.css';
+import { ErrorPage } from './pages/ErrorPage';
+import { HomePage } from './pages/HomePage';
+import { ReservationPage } from './components/ReservationPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    errorElement : <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <HomePage />,
+      },
+      {
+            path: '/reservation',
+            element: <ReservationPage />,
+      },
+    ],
   },
 ]);
 
