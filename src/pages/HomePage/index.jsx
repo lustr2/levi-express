3ab406ca69481da71dd270304fad020c2eddb0c0
Lucie Struplova <lstruplova@gmail.com>
2024-04-13 12:58:35 +0200

    3 Potvrzeni registrace (1-2) / bez useNavigate

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -2,15 +2,22 @@ import { JourneyDetail } from '../../components/JourneyDetail';
 import { JourneyPicker } from '../../components/JourneyPicker';
 import { useState } from 'react';
 import { SelectedSeat } from '../../components/SelectedSeat';
+import { useNavigate } from "react-router-dom";
+
 
 export const HomePage = () => {
   const [journey, setJourney] = useState(null);
+  const navigate = useNavigate();
 
   const handleJourneyChange = (cesta) => {
     console.log("Poslano do rodice: " + cesta.journeyId);
 //    alert(cesta.code);
     setJourney(cesta);
   };
+
+  const handleBuy = () => {
+    console.log('Funguju!!');
+  }
 
   return (
     <main>
@@ -19,6 +26,11 @@ export const HomePage = () => {
         <>
           <JourneyDetail journey={journey?.stops} />
           <SelectedSeat number={journey?.autoSeat} />
+          <div className="controls container">
+            <button className="btn btn--big" type="button" onClick={handleBuy}>Rezervovat</button>
+          </div>
+
+
         </>}
       
     </main>
